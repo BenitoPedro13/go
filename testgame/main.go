c1ac7cc0d526041b4b848be628eb7d8e36b4f957
Benito Pedro <benitopedro21@outlook.com>
2025-06-22 15:39:23 -0300

    Add tile map rendering and initialization logic
    - Introduced tileMap and related variables for managing the game map.
    - Implemented loadMap function to initialize the tileMap with default values.
    - Updated drawScene function to render tiles based on the tileMap.
    - Set up source and destination rectangles for tile rendering in the init function.

--- testgame/main.go
+++ testgame/main.go
@@ -23,6 +23,12 @@ var (
 
 	frameCount int
 
+	tileDest   rl.Rectangle
+	tileSrc    rl.Rectangle
+	tileMap    []int
+	srcMap     []string
+	mapW, mapH int
+
 	playerSpeed float32 = 3
 
 	musicPaused bool
@@ -32,7 +38,20 @@ var (
 )
 
 func drawScene() {
-	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	// rl.DrawTexture(grassSprite, 100, 50, rl.White)
+
+	for i := 0; i < len(tileMap); i++ {
+		if tileMap[i] != 0 {
+			tileDest.X = tileDest.Width * float32(i%mapW)
+			tileDest.Y = tileDest.Height * float32(i/mapW)
+
+			tileSrc.X = tileSrc.Width * float32((tileMap[i]-1)%int(grassSprite.Width/int32(tileSrc.Width)))
+			tileSrc.Y = tileSrc.Height * float32((tileMap[i]-1)/int(grassSprite.Width/int32(tileSrc.Width)))
+
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(tileDest.Width, tileDest.Height), 0, rl.White)
+		}
+	}
+
 	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
 }
 
@@ -138,6 +157,14 @@ func render() {
 	rl.EndDrawing()
 }
 
+func loadMap() {
+	mapW = 5
+	mapH = 5
+	for i := 0; i < (mapW * mapH); i++ {
+		tileMap = append(tileMap, 1)
+	}
+}
+
 func init() {
 	// Initialize window
 	rl.InitWindow(screenWidth, screenHeight, "Benitolandia")
@@ -146,6 +173,11 @@ func init() {
 
 	// Load textures
 	grassSprite = rl.LoadTexture("res/Tilesets/Grass.png")
+
+	// Set tile source and destination rectangles
+	tileDest = rl.NewRectangle(0, 0, 16, 16)
+	tileSrc = rl.NewRectangle(0, 0, 16, 16)
+
 	playerSprite = rl.LoadTexture("res/Characters/BasicCharakterSpritesheet.png")
 
 	// Set player source rectangle
@@ -163,8 +195,11 @@ func init() {
 		rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
 		rl.NewVector2(playerDest.X-playerDest.Width/2, playerDest.Y-playerDest.Height/2),
 		0.0,
-		1.0,
+		1.5,
 	)
+
+	// Load map
+	loadMap()
 }
 
 func quit() {
